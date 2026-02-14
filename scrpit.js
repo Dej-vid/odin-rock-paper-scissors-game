c01b942bb01dd6e55473ab0fcc98625df7a1f1ef
Dawid.M <allowcreativepr@gmail.com>
2026-02-14 15:49:04 +0000

    Added function for computer choice
    - Added a random number generator between 1 and 3
    - Assigned values for each number generated
    - choice printed to console

--- scrpit.js
+++ scrpit.js
@@ -1,1 +1,16 @@
-console.log("Hello")
+// Function to return rock, paper or scissors
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3) + 1;
+
+    if (choice === 1) {
+        console.log("Rock");
+    } else if (choice === 2) {
+        console.log("Paper");
+    } else {
+        console.log("Scissors");
+    }
+}
+
+getComputerChoice();
+
